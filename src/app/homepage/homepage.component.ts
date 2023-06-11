7af63397587d9333a2a91d3ac08afaa92861021f
Nour Ashoush <nour.bsm.ashoush@gmail.com>
2023-06-11 15:11:15 +0300

    Added progress bar to home page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent { }
+export class HomepageComponent implements OnInit, OnDestroy {
+  ngOnInit() {
+    window.addEventListener('scroll', this.updateProgressBar.bind(this));
+
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.updateProgressBar.bind(this));
+  }
+
+  private updateProgressBar(): void {
+    const {scrollTop, scrollHeight} = document.documentElement;
+    const scrollPercent = `${scrollTop / (scrollHeight - window.innerHeight) * 100}%`;
+    const progressBar = document.querySelector('.progress-bar') as HTMLElement;
+    progressBar.style.setProperty('--progress', scrollPercent);
+  }
+
+}
