c778eb56ee858be21052e7c2ae1d1736c749c175
Nour Ashoush <nour.bsm.ashoush@gmail.com>
2023-06-16 00:10:19 +0300

    Added education dropdown functionality + Rebuild

--- src/app/homepage/education/education.component.ts
+++ src/app/homepage/education/education.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-education',
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent {
+  pageElements = ["tech_skills", "education", "projects"];
+  showModules: boolean = false;
+
+  toggleShowModules(): void {
+    this.showModules = !this.showModules;
+    const modulesElement = document.querySelector('.academy-modules') as HTMLElement;
+    const height = modulesElement.scrollHeight + 'px';
+    const showArrow = document.querySelector('#show-arrow') as HTMLElement;
+
+    if (this.showModules) {
+      modulesElement.style.setProperty('max-height', height);
+      showArrow.style.setProperty('transform', 'rotate(180deg)');
+      setTimeout(() => {
+        this.updateProgressBar();
+        this.setCheckPointHeights();
+      }, 500);
+    } else {
+      modulesElement.style.setProperty('max-height', '0');
+      showArrow.style.setProperty('transform', 'rotate(0deg)');
+      setTimeout(() => {
+        this.updateProgressBar();
+        this.setCheckPointHeights();
+      }, 500);
+    }
+  }
+
+  private updateProgressBar(): void {
+    const {scrollTop, scrollHeight} = document.documentElement;
+    const scrollPercent = `${scrollTop / (scrollHeight - window.innerHeight) * 100}%`;
+    const progressBar = document.querySelector('.progress-bar') as HTMLElement;
+    progressBar.style.setProperty('--progress', scrollPercent);
+  }
+
+  private setCheckPointHeights(): void {
+    const {scrollHeight} = document.documentElement;
+
+    for (let i = 0; i < this.pageElements.length; i++) {
+      const pageElement = document.querySelector(`#${this.pageElements[i]}_element`) as HTMLElement;
+      const equivCheckpoint = document.querySelector(`#${this.pageElements[i]}_checkpoint`) as HTMLElement;
+      const heightPercent = `${(pageElement.offsetTop / (scrollHeight - window.innerHeight) * 100) - 1}%`;
+      equivCheckpoint.style.setProperty('--heightOffset', heightPercent);
+    }
+  }
 
 }
