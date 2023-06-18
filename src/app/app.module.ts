5c13721e1779d06ca93337e7fbc2fca9479aeebc
Nour Ashoush <nour.bsm.ashoush@gmail.com>
2023-06-18 21:37:14 +0300

    Moved footer component + rebuild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeMessageComponent } from './homepage/welcome-message/welcome-message.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 import { TechSkillsComponent } from './homepage/tech-skills/tech-skills.component';
 import { EducationComponent } from './homepage/education/education.component';
 import { ProjectsComponent } from './homepage/projects/projects.component';
