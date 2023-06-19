7821f50d1de1bdddd3cdeed4291fec406f669a65
Nour Ashoush <nour.bsm.ashoush@gmail.com>
2023-06-19 18:34:00 +0300

    Formatted projects & added auto dark mode + rebuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-personal-portfolio';
+  darkMode: boolean | undefined;
+
+  ngOnInit() {
+    this.detectDarkModePreference();
+  }
+
+  detectDarkModePreference() {
+    this.darkMode = this.isDarkModePreferred();
+    this.toggleDarkMode();
+  }
+
+  private isDarkModePreferred(): boolean {
+    // Check if the user has a dark mode preference
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  toggleDarkMode() {
+    if (this.darkMode) {
+      document.documentElement.classList.toggle('dark-mode');
+    }
+  }
 }
