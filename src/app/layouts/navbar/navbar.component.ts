0889f0d7a2962dbab22a9586376486f9a43369f5
Nour Ashoush <nour.bsm.ashoush@gmail.com>
2023-06-24 23:40:33 +0300

    Mobile interface update + rebuild

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  public showNav(): void {
+    (document.querySelector(".nav-button") as HTMLElement).classList.add('hide');
+    (document.querySelector(".overlay") as HTMLElement).classList.remove('hide');
+  }
 
+  public hideNav(): void {
+    (document.querySelector(".nav-button") as HTMLElement).classList.remove('hide');
+    (document.querySelector(".overlay") as HTMLElement).classList.add('hide');
+  }
+
+  public scrollToElement(elementId: string): void {
+    // @ts-ignore
+    document.querySelector(`#${elementId}`).scrollIntoView();
+  }
 }
